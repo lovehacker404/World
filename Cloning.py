82696bfd03edbc5d4be11f2def132b32b6fff468
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-05-06 20:41:46 +0500

    Update Cloning.py

--- Cloning.py
+++ Cloning.py
@@ -22,21 +22,6 @@ def keluar():
 	print "\x1b[1;91mExit"
 	os.sys.exit()
 
-def main():
- 	DATETIME OBJECTS
-        Get today's date from datetime class
-	today=datetime.now()
-	print today
-	Get the current time
-	t = datetime.time(datetime.now())
-	print "The current time is", t
-	weekday returns 0 (monday) through 6 (sunday)
-        wd = date.weekday(today)
-	Days start at 0 for monday
-        days= ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
-        print "Today is day number %d" % wd
-        print "which is a " + days[wd]
-
 def acak(b):
     w = 'ahtdzjc'
     d = ''
