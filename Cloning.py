c7c0cb3779073e36b6a8e62e18cf66700347dcc5
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-05-07 23:14:15 +0500

    Update Cloning.py

--- Cloning.py
+++ Cloning.py
@@ -27,7 +27,7 @@ br.addheaders = [('User-Agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_3_2 like M
 br.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36')]
 br.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36')]
 br.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36')]
-br.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36'])]
+br.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36')]
 
 def keluar():
 	print "\x1b[1;91mExit"
