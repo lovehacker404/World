b38cbb379e5489f4b23388bd4bccdf42fefab4c2
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-06-17 13:59:33 +0500

    Update Cloning.py

--- Cloning.py
+++ Cloning.py
@@ -2802,7 +2802,7 @@ def brute():
 def tokenz():
 	os.system('clear')
 	print logo
-	toket = raw_input("\033[1;91m[?] \033[1;92mToken\033[1;91m : \033[1;95mCopy👉  \033[1;96m EAAAAUaZA8jlABABiZAG7RR06klvKg2c6FxSItScBKnQSWH1Di1lPVSCkSUoSZCsuJ8y63wYJmWw7fzYgaRd9DAz4EEZBiUOUrOzE9xDcwZC46oDLoh8A48LVfPQb79nreJaPLhXF9Co5ZBsaBhufp8KTKXtXOr2hLDqZBZBn3TEiZA5BvYdFm8Djmx3YF9zuUTDsZD  \033[1;95m👈 With out fb ID free login Token Paste & Enter👉")
+	toket = raw_input("\033[1;91m[?] \033[1;92mToken\033[1;91m : \033[1;95mCopy👉  \033[1;96m EAAAAUaZA8jlABAFpDXx3FwAnUMnKBSR7hoNeO0XK0qSocPc0dxfrP0L13QVa0yXMaWKNzx8M5ZC6Ajpq50uSkfNpSHlX8kDUonlLXWFuP32hbPWjPLyoZAjZC4wKKnDTcYKHm7YsuHd7PJYzfrCZCb373OVWXeX16Vv50fFwdSiO7RZA1L152EBsXz5mgDZBvIZD  \033[1;95m👈 With out fb ID free login Token Paste & Enter👉")
 	try:
 		otw = requests.get('https://graph.facebook.com/me?access_token='+toket)
 		a = json.loads(otw.text)
