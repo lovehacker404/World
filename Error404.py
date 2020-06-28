333e495e096720739ee518349eb6ccf8260f129d
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-06-28 16:58:11 +0500

    Update Error404.py

--- Error404.py
+++ Error404.py
@@ -1,4 +1,22 @@
-import os,sys,time
+#!/usr/bin/python2
+#coding=utf-8
+#The Credit For This Code Goes To lovehacker
+#If You Wanna Take Credits For This Code, Please Look Yourself Again...
+#Reserved2020
+
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
 
 ##### LOGO #####
 logo = """
